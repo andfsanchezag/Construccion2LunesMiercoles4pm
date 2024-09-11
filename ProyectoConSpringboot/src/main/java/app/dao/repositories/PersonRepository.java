4d75b9e4bc93b5254da6b27fd9ac8e0245386706
andfsanchezag <andres.sanchez57@tdea.edu.co>
2024-09-11 17:14:51 -0500

    add

---
+++ ProyectoConSpringboot/src/main/java/app/dao/repositories/PersonRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package app.dao.repositories;
+
+import app.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public interface PersonRepository extends JpaRepository<Person,Long> {
+
+    public boolean existsByDocument(Long document);
+    public Person findByDocument(Long document);
+    
+    
+}
